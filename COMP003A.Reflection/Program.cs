ece1e2b2aabe538a0c4ead85d236b74d9d0dcab9
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-02-20 20:06:50 -0800

    Reflection.

--- COMP003A.Reflection/Program.cs
+++ COMP003A.Reflection/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace COMP003A.Reflection;
+﻿/*
+    Author: Angel Gomez
+    Course: COMP-003A
+    Faculty: Jonathan Cruz
+    Purpose: Series of console outputs with the questions followed by responses
+*/
+namespace COMP003A.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("1. What worked well for you over the last 8 weeks? ");
+        Console.WriteLine("Learning the basic concept of programming and learning how to read and code in C#. ");
+        
+        Console.WriteLine("\n2. What external resources did you use to aid your learning? ");
+        Console.WriteLine("An external resource that I used was 'YouTube' to better understand some concepts. ");
+        
+        Console.WriteLine("\n3. What would you change about the course? "); 
+        Console.WriteLine("I would change the due dates of the weekly coding assignments by either increasing the due date or opening them a day earlier (Tuesdays). ");
+        
+        Console.WriteLine("\n4. Did you learn what you expected to learn in this course? ");
+        Console.Write("Not fully, but I definitely am glad to have learned C#. I would have liked to learn how to ");
+        Console.Write("apply the console applications that we create into real situations like websites, applications,");
+        Console.Write(" games, etc. (not exactly sure what it's called) because prior to this class I had basically ");
+        Console.WriteLine("never seen a console and I don't know what to do with my code outside of Rider/Visial Studio.");
+        
+        Console.WriteLine("\n5. What concepts were new to you? ");
+        Console.WriteLine("Pretty much all the concepts in this course were new to me. "); 
+        
+        Console.WriteLine("\n6. What concepts have you encountered already in the past? ");
+        Console.Write("The only concepts that I had already encountered in the past are the mathematical ones like ");
+        Console.WriteLine("expressions and the if-else conditional statements (from statistics and calculus). ");
+        
+        Console.WriteLine("\n7. Do you have any tips/comments for the instructor? ");
+        Console.WriteLine("Probably add at least one extra credit opportunity. "); 
+        
     }
 }
